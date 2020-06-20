53a907f05eda7566ab55047cea00d221e5d50566
Cooper Saye <coopersaye@gmail.com>
2020-06-19 19:00:47 -0700

    Create save data class and example player data.

---
+++ Assets/SaveLoadSystem/Scripts/SaveLoad/SaveData/PlayerData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace SaveLoadSystem.SaveLoad
+{
+    public class PlayerData
+    {
+        public string playerName;
+        public int playerPoints;
+        public Vector2 playerPosition;
+
+        public PlayerData(string _playerName, int _playerPoints, Vector2 _playerPosition)
+        {
+            playerName = _playerName;
+            playerPoints = _playerPoints;
+            playerPosition = _playerPosition;
+        }
+    }
+}
