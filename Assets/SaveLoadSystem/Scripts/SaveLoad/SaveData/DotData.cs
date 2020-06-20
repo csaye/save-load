2ffcb177799cc7e1b4fc7ad6d728fd0db5785ff1
Cooper Saye <coopersaye@gmail.com>
2020-06-20 09:51:34 -0700

    Create dot data class and Vector2 surrogate.

---
+++ Assets/SaveLoadSystem/Scripts/SaveLoad/SaveData/DotData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace SaveLoadSystem.SaveLoad
+{
+    public class DotData
+    {
+        public Vector2 dotPosition;
+
+        public DotData(Vector2 _dotPosition)
+        {
+            dotPosition = _dotPosition;
+        }
+    }
+}
